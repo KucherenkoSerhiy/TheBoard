fdcdf97276ceb0d68b53d78ce1cce8355de11605
Serhiy K <skucherenko@barberan.com>
2019-12-02 23:22:11 +0100

    Implemented the get of notes from database

---
+++ TheBoard/public/js/notesView.js
@@ -0,0 +1,24 @@
+﻿(function (angular) {
+    var theModule = angular.module("notesView", []);
+
+    theModule.controller("notesViewController",
+        ["$scope", "$window", "$http",
+            function ($scope, $window, $http) {
+                $scope.notes = [];
+
+                //Get the category name
+                var urlParts = $window.location.pathname.split("/");
+                var categoryName = urlParts[urlParts.length - 1];
+
+                var notesUrl = "/api/notes/" + categoryName;
+                $http.get(notesUrl)
+                    .then(function(result) {
+                        // success
+                        $scope.notes = result.data;
+                    }, function(err) {
+                        // error
+                        alert(err);
+                    });
+            }
+        ]);
+})(window.angular)
